7cc41c5e56c61b686ba478d8b4027cf229a71489
Ujwal Kumar <ujwalkumar95@gmail.com>
2019-07-27 18:44:49 +0530

    First Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+
 
 class App extends React.Component {
   render() {
@@ -12,6 +14,7 @@ class App extends React.Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu/>
       </div>
     );
   }
