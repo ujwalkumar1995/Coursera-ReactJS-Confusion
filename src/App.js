2d0aa98cdf887eb884d071cf316d68c1c32d8b41
Ujwal Kumar <ujwalkumar95@gmail.com>
2019-07-27 18:33:25 +0530

    Configuring React

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
